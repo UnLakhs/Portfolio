3ed7f71be50543b49652994b7cb76c11d2bb8198
UnLakhs <apostolos052@gmail.com>
2026-04-19 20:08:07 +0200

    body bacground color

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full">
+      <body className="min-h-full bg-background">
         <div className="flex gap-10">
           <Nav />
           <div className="mt-6">
