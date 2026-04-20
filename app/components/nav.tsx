6183cd46eeb252a245218009e5403d3bbe90c9cf
UnLakhs <apostolos052@gmail.com>
2026-04-20 16:27:06 +0300

    added links

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,13 +4,13 @@ const Nav = () => {
             <div className="flex gap-1 flex-col ml-6">
                 <h4 className="font-semibold p-2 rounded-md text-lg">Sections</h4>
                 <div className="flex flex-col text-lg gap-1">
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Introduction</span>
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">About Me</span>
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Projects</span>
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Skills & tools</span>
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Experience</span>
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Education</span>
-                    <span className="hover:bg-gray-400 p-2 rounded-md transition duration-200">Contact</span>
+                    <a href="/" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">Introduction</a>
+                    <a href="./AboutMe" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">About Me</a>
+                    <a href="./Projects" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">Projects</a>
+                    <a href="./Skills" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">Skills & tools</a>
+                    <a href="./Experience" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">Experience</a>
+                    <a href="./Education" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">Education</a>
+                    <a href="./Contact" className="hover:bg-gray-300 p-2 rounded-md transition duration-200">Contact</a>
                 </div>
 
             </div>
